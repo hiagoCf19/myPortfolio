7b89611b7f949acd55079f144484fe0132a1fa5e
hiagoCf19 <hiagoferreira@id.uff.br>
2024-05-20 18:10:50 -0300

    fix: ajustando cn

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,8 +4,10 @@ import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
-
 import { cn } from "@/lib/utils"
+
+
+
 
 const Sheet = SheetPrimitive.Root
 
